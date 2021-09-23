537bf6e72db0ae78da46d42bddcd44b599ba7671
Paulo Xavier <pxavierf@gmail.com>
2021-09-23 15:17:05 -0300

    insert form info - miragejs schema

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { api } from '../../services/api';
 import { ButtonType, Container, TransactionTypeContainer } from './styles';
 
 interface NewTransacionModalProps {
@@ -11,7 +12,23 @@ interface NewTransacionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransacionModalProps) {
+  const [title, setTitle] = useState('')
+  const [value, setValue] = useState(0)
+  const [category, setCategory] = useState('')
   const [type, setType] = useState('deposit');
+
+  function heandleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    }
+
+    api.post('/transactions', data)
+  }
 
   return (
     <Modal
@@ -23,11 +40,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransacionMod
       <button type="button" onClick={onRequestClose} className="react-modal-close">
         <img src={closeImg} alt="Fechar modal" />
       </button>
-      <Container>
+      <Container onSubmit={heandleCreateNewTransaction}>
         <h2>Cadastrar</h2>
 
-        <input type="text" placeholder="Título" />
-        <input type="number" placeholder="Valor" />
+        <input
+          type="text"
+          placeholder="Título"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={(e) => setValue(Number(e.target.value))}
+        />
 
         <TransactionTypeContainer>
           <ButtonType
@@ -56,7 +83,12 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransacionMod
           </ButtonType>
         </TransactionTypeContainer>
 
-        <input type="number" placeholder="Categoria" />
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
         <button type="submit">
           Cadastrar
         </button>
